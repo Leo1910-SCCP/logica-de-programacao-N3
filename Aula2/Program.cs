54a0d61ceea29bf987143991dbd7299b88d7a851
LEONARDO LOPES DOS SANTOS <leonardo.lsantos21@senacsp.edu.br>
2025-11-03 21:36:37 -0300

    primeiro commit

---
+++ Aula2/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string nome;
+
+nome = "Leo";
+Console.WriteLine(nome);
+
+nome = "Leo Lopes";
+
+Console.WriteLine(nome);
+
+const string constante = "Minha constante";
+
+
+Console.WriteLine(constante);
+
+// Típo nome = valor;
+
+int Idade = 20;
+float altura = 1.01f;
+Double Altura2 = 1.9;
+char Caracter = 'A';
+long Inteiro = 10;
+bool Boleano = false;
+
+
+
+
+Console.WriteLine(Inteiro);
+Console.WriteLine(Caracter);
+Console.WriteLine(Idade);
+Console.WriteLine(altura);
+Console.WriteLine(Altura2);
