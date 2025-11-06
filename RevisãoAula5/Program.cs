6e59e0df58e0bd9332b8d61ccec164a1f1bf7675
LEONARDO LOPES DOS SANTOS <leonardo.lsantos21@senacsp.edu.br>
2025-11-05 21:58:40 -0300

    Aula5

---
+++ RevisãoAula5/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization;
+
+int n1;
+int n2;
+
+
+
+//Console.WriteLine("Insira um numero inteiro:");
+//n1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Insira outro numero:");
+//n2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Voce digitou " + n1 + " E " + n2);
+
+
+//Console.WriteLine("Insira um numero inteiro:");
+//n1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Insira outro numero:");
+//n2 = int.Parse(Console.ReadLine());
+//int Soma = n1 + n2;
+//Console.WriteLine("A soma dos numeros é " + Soma);
+//int Sub = n1 - n2;
+//Console.WriteLine("A subtração dos numeros é " + Sub);
+
+Console.WriteLine("Insira um numero inteiro:");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Insira outro numero:");
+n2 = int.Parse(Console.ReadLine());
+int Soma = n1 + n2;
+Console.WriteLine("A soma dos numeros é " + Soma);
+int Sub = n1 - n2;
+Console.WriteLine("A subtração dos numeros é " + Sub);
