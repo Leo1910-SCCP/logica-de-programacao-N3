bc3dcc7cc1a62d29224f6f64e34aca93240d8475
LEONARDO LOPES DOS SANTOS <leonardo.lsantos21@senacsp.edu.br>
2025-11-10 21:53:35 -0300

    Aula 6

---
+++ Aula6/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System.ComponentModel.Design;
+
+Console.WriteLine("qual sua idade");
+
+string Resposta  = Console.ReadLine();
+
+int Idade = int.Parse(Resposta);
+
+bool Valido = int.TryParse(Resposta, out Idade);
+
+
+
+if (Valido)
+{
+ 
+    
+    if (Idade >= 18)
+            
+     {   Console.WriteLine("Usuario maior de idade");   
+     }
+ 
+     else if (Idade <=0)
+     {
+        Console.WriteLine("Idade invalida"); 
+     }   
+     
+    else 
+    {
+        Console.WriteLine("Usuario menor de idade");
+    }
+        
+
+
+
+
+
+}
+           
+else 
+        {
+            Console.WriteLine("valor informado não é valido");
+
+        }       
+        
+
+
+
+
+
+
+
+
+
+
+
+
